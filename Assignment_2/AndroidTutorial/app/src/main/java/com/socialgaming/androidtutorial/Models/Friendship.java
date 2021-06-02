5761a7f1491fd66d1106dac21709de768ca32815
Maxi <ge93tog@mytum.de>
2021-06-02 12:06:58 +0200

    updated FriendshipStart creation in Friendship.java

--- Assignment_2/AndroidTutorial/app/src/main/java/com/socialgaming/androidtutorial/Models/Friendship.java
+++ Assignment_2/AndroidTutorial/app/src/main/java/com/socialgaming/androidtutorial/Models/Friendship.java
@@ -16,20 +16,22 @@ public class Friendship {
     public String friendOne;
     public String friendTwo;
     public FriendshipRank rank; //je nach Rank anderen Multiplier beim traden etc.
-    public LocalDate friendshipStart;
+    public int dayOfYear,year;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public Friendship(String du, String freund){
         friendOne=du;
         friendTwo=freund;
         rank=FriendshipRank.FRIENDLY_GREETINGS;//Default startwert, soll immer dann aktualisiert werden wenn freundesliste geöffnet wird
-        friendshipStart=java.time.LocalDate.now();
+        LocalDate friendshipStart=java.time.LocalDate.now();
+        dayOfYear=friendshipStart.getDayOfYear();
+        year=friendshipStart.getYear();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void updateRank(){
         LocalDate today=java.time.LocalDate.now();
-        long daysOfFriendship= ChronoUnit.DAYS.between(friendshipStart,today);
+        long daysOfFriendship= ChronoUnit.DAYS.between(LocalDate.ofYearDay(year,dayOfYear),today);
         //TODO Tage-Rang Verhältnis anpassen
         if(daysOfFriendship<=2)
             rank=FriendshipRank.FRIENDLY_GREETINGS;
