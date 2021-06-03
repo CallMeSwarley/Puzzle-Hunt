87fc1bd594738e7a0ca3faddbce86c58bec62640
Laura_Marsoner <laura.marsoner@hotmail.com>
2021-06-03 13:03:05 +0200

    added puzzlemap with weather conditions and icons dynamically updated when opening activity

--- Assignment_2/AndroidTutorial/app/src/main/java/com/socialgaming/androidtutorial/PuzzleMapActivity.java
+++ Assignment_2/AndroidTutorial/app/src/main/java/com/socialgaming/androidtutorial/PuzzleMapActivity.java
@@ -1,14 +1,286 @@
 package com.socialgaming.androidtutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class PuzzleMapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.auth.FirebaseAuth;
+import com.socialgaming.androidtutorial.Models.Weather;
+import com.socialgaming.androidtutorial.Models.Location;
+import com.socialgaming.androidtutorial.Util.HTTPPoster;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+//Weather implementation idee: https://github.com/survivingwithandroid/Swa-app/blob/master/WeatherApp/src/com/survivingwithandroid/weatherapp/MainActivity.java,
+//wurde angepasst
+
+public class PuzzleMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    int MY_RESULT_FINE_LOCATION;
+    private static String url = "http://api.openweathermap.org/data/2.5/weather?";
+    private static String imgUrl = "http://openweathermap.org/img/wn/";
+    private static String appid = "858fcdc021157c5dd2e1cd35925ae125";
+
+    private TextView cityText;
+    private TextView condDescr;
+    private ImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map2);
+        mapFragment.getMapAsync(this);
+
+
+        //kann mit city oder lat,lon aufgerufen werden (LIMIT 60 mal/h
+        //String city = "Munich,DE";
+        cityText = findViewById(R.id.cityText);
+        condDescr = findViewById(R.id.condDescr);
+        imgView =  findViewById(R.id.condIcon);
+        String lat = "48.13743";
+        String lon = "11.57549";
+        JSONWeatherTask task = new JSONWeatherTask();
+
+        task.execute(new String[]{lat, lon});
+        //task.execute(new String[]{city});
+
     }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+       /* LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+*/
+        if (ContextCompat.checkSelfPermission(PuzzleMapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(PuzzleMapActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_RESULT_FINE_LOCATION);
+        } else {
+            mMap.setMyLocationEnabled(true);
+            if (mMap != null) {
+                mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+                    @Override
+                    public void onMyLocationChange(android.location.Location location) {
+                        mMap.clear();
+                        LatLng user = new LatLng(location.getLatitude(), location.getLongitude());
+                        new HTTPPoster().execute(
+                                "position",
+                                FirebaseAuth.getInstance().getUid(),
+                                "" + user.latitude,
+                                "" + user.longitude,
+                                "update");
+                    }
+                });
+            }
+        }
+
+    }
+
+    public String getWeatherDataWithCity(String location) {
+        HttpURLConnection con = null ;
+        InputStream is = null;
+
+        try {
+            con = (HttpURLConnection) ( new URL(url +"q="+ location+"&appid="+appid)).openConnection();
+            con.setRequestMethod("GET");
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.connect();
+
+            // Let's read the response
+            StringBuffer buffer = new StringBuffer();
+            is = con.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line = null;
+            while (  (line = br.readLine()) != null )
+                buffer.append(line + "\r\n");
+
+            is.close();
+            con.disconnect();
+            return buffer.toString();
+        }
+        catch(Throwable t) {
+            t.printStackTrace();
+        }
+        finally {
+            try { is.close(); } catch(Throwable t) {}
+            try { con.disconnect(); } catch(Throwable t) {}
+        }
+
+        return null;
+
+    }
+
+    public String getWeatherDataWithLatAndLon(String lat, String lon) {
+        HttpURLConnection con = null ;
+        InputStream is = null;
+
+        try {
+            con = (HttpURLConnection) ( new URL(url +"lat="+ lat+"&lon="+lon+"&appid="+appid)).openConnection();
+            con.setRequestMethod("GET");
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.connect();
+
+            // Let's read the response
+            StringBuffer buffer = new StringBuffer();
+            is = con.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line = null;
+            while (  (line = br.readLine()) != null )
+                buffer.append(line + "\r\n");
+
+            is.close();
+            con.disconnect();
+            return buffer.toString();
+        }
+        catch(Throwable t) {
+            t.printStackTrace();
+        }
+        finally {
+            try { is.close(); } catch(Throwable t) {}
+            try { con.disconnect(); } catch(Throwable t) {}
+        }
+
+        return null;
+
+    }
+
+    private class JSONWeatherTask extends AsyncTask<String, Void, Weather> {
+
+        @Override
+        protected Weather doInBackground(String... params) {
+            Weather weather = new Weather();
+            String data="";
+            if(params.length>1) {
+                data = (getWeatherDataWithLatAndLon(params[0], params[1]));
+            }
+            else{
+                data = (getWeatherDataWithCity(params[0]));
+            }
+
+            try {
+                weather = getWeather(data);
+
+                // Let's retrieve the icon
+                Bitmap bmp = null;
+                try {
+                    InputStream in = new java.net.URL(imgUrl + weather.currentCondition.getIcon()+"@2x.png").openStream();
+                    bmp = BitmapFactory.decodeStream(in);
+
+                } catch (Exception e) {
+                    Log.e("Error", e.getMessage());
+                    e.printStackTrace();
+                }
+                weather.iconData =  bmp;
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return weather;
+        }
+
+        //renders icon, city and condition
+        @Override
+        protected void onPostExecute(Weather weather) {
+            super.onPostExecute(weather);
+            imgView.setImageBitmap(weather.iconData);
+            cityText.setText(weather.location.getCity() + "," + weather.location.getCountry());
+            condDescr.setText(weather.currentCondition.getCondition() + "(" + weather.currentCondition.getDescr() + ")");
+        }
+
+        protected Weather getWeather(String data) throws JSONException  {
+            Weather weather = new Weather();
+
+            // We create out JSONObject from the data
+            JSONObject jObj = new JSONObject(data);
+
+            // We start extracting the info
+            Location loc = new Location();
+
+            JSONObject coordObj = getObject("coord", jObj);
+            loc.setLatitude(getFloat("lat", coordObj));
+            loc.setLongitude(getFloat("lon", coordObj));
+
+            JSONObject sysObj = getObject("sys", jObj);
+            loc.setCountry(getString("country", sysObj));
+            loc.setCity(getString("name", jObj));
+            weather.location = loc;
+
+            // We get weather info (This is an array)
+            JSONArray jArr = jObj.getJSONArray("weather");
+
+            // We use only the first value
+            JSONObject JSONWeather = jArr.getJSONObject(0);
+            weather.currentCondition.setWeatherId(getInt("id", JSONWeather));
+            weather.currentCondition.setDescr(getString("description", JSONWeather));
+            weather.currentCondition.setCondition(getString("main", JSONWeather));
+            weather.currentCondition.setIcon(getString("icon", JSONWeather));
+
+            JSONObject mainObj = getObject("main", jObj);
+
+            return weather;
+        }
+
+
+        private JSONObject getObject(String tagName, JSONObject jObj)  throws JSONException {
+            JSONObject subObj = jObj.getJSONObject(tagName);
+            return subObj;
+        }
+
+        private String getString(String tagName, JSONObject jObj) throws JSONException {
+            return jObj.getString(tagName);
+        }
+
+        private float  getFloat(String tagName, JSONObject jObj) throws JSONException {
+            return (float) jObj.getDouble(tagName);
+        }
+
+        private int  getInt(String tagName, JSONObject jObj) throws JSONException {
+            return jObj.getInt(tagName);
+        }
+
+    }
+
+
 }
