cd4969ab39116655376d82652540709046500845
Yi Rui Cui <yirui.cui@gmail.com>
2021-06-03 13:09:52 +0200

    removed places api

--- Assignment_2/AndroidTutorial/app/src/main/java/com/socialgaming/androidtutorial/Models/Puzzle.java
+++ Assignment_2/AndroidTutorial/app/src/main/java/com/socialgaming/androidtutorial/Models/Puzzle.java
@@ -4,12 +4,6 @@ import android.graphics.Bitmap;
 import android.util.Log;
 
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.model.PhotoMetadata;
-import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.net.FetchPhotoRequest;
-import com.google.android.libraries.places.api.net.FetchPlaceRequest;
-import com.google.android.libraries.places.api.net.PlacesClient;
 
 import java.util.Collections;
 import java.util.List;
